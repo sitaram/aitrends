86156a72bfbca0c84c146db2876e21bc6b155452
Sitaram Iyer <sitaram@gmail.com>
2024-02-03 02:35:49 -0800

    md

--- app/CustomMarkdown.js
+++ app/CustomMarkdown.js
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 const CustomMarkdown = ({ text }) => {
   // Function to replace bold markdown with <b> tags
   const replaceBoldMarkdown = (markdownText) => {
-    const boldRegex = /(\*\*|__)(.*?)\1/g;
+    const boldRegex = /(\*\*?|__)(.*?)\1/g;
     const sectionRegex = /#+ (\d+\.)?(.*)/g;
     const bulletRegex = /^ *- (.*)/g;
     return (markdownText || '')
