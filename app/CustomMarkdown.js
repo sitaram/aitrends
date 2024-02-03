6246207463aef674b799c875410a0ede20b58462
Sitaram Iyer <sitaram@gmail.com>
2024-02-03 09:52:39 -0800

    links

--- app/CustomMarkdown.js
+++ app/CustomMarkdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const CustomMarkdown = ({ text, topic }) => {
-  // Function to replace bold markdown with <b> tags
+  // Function to replace bold markdown with <b> tags and link with hover effect
   const replaceBoldMarkdown = (markdownText) => {
     const boldRegex = /(\*\*?|__)(.*?)\1/g;
     const sectionRegex = /#+ (\d+\.)?(.*)/g;
@@ -11,21 +11,20 @@ const CustomMarkdown = ({ text, topic }) => {
       .replace(
         boldRegex,
         (match, p1, p2) =>
-          `<b><a href="https://www.google.com/search?q=${encodeURIComponent(
+          `<b><a class="custom-link" href="https://www.google.com/search?q=${encodeURIComponent(
             topic + ': ' + p2
           )}" target="_blank">${p2}</a></b>`
       )
       .replace(
         sectionRegex,
         (match, p1, p2) =>
-          `<u><b><a href="https://www.google.com/search?q=${encodeURIComponent(
+          `<u><b><a class="custom-link" href="https://www.google.com/search?q=${encodeURIComponent(
             topic + ': ' + p2
           )}" target="_blank">${p2}</a></b></u>`
       )
       .replace(bulletRegex, '&emsp;- $1');
   };
 
-  // Replace markdown in the text with HTML
   const htmlContent = replaceBoldMarkdown(text);
 
   return <div dangerouslySetInnerHTML={{ __html: htmlContent }} />;
