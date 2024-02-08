0353b1c34664cd8a1176c539838c9619e0a9a726
Sitaram Iyer <sitaram@gmail.com>
2024-02-07 20:58:21 -0800

    tip

--- app/SwipeTooltip.js
+++ app/SwipeTooltip.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { theme } from './theme';
 
 const SwipeTooltip = ({ topicsDrawerOpen }) => {
   const [visible, setVisible] = useState(false);
@@ -8,7 +9,7 @@ const SwipeTooltip = ({ topicsDrawerOpen }) => {
       setVisible(true);
       const timer = setTimeout(() => {
         setVisible(false); // Optionally hide after 2 seconds
-      }, 2000);
+      }, 2500);
       return () => clearTimeout(timer);
     }
   }, [topicsDrawerOpen]);
@@ -18,13 +19,13 @@ const SwipeTooltip = ({ topicsDrawerOpen }) => {
   const tooltipStyle = {
     position: 'fixed',
     bottom: '20%', // Adjust as needed
-    width: '270px',
+    width: '320px',
     left: '50%',
     transform: 'translateX(-50%)',
-    backgroundColor: 'rgba(0,0,0,0.7)',
-    color: 'white',
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.text.main,
     padding: '16px 16px',
-    borderRadius: '4px',
+    borderRadius: '12px',
     fontSize: '1rem',
     transition: 'opacity 0.5s ease',
     boxShadow: '0 2px 8px rgba(0,0,0,0.2)',
@@ -35,10 +36,8 @@ const SwipeTooltip = ({ topicsDrawerOpen }) => {
 
   return (
     <div style={tooltipStyle}>
-      Swipe left/right to switch between tabs and then topics.
-      <br />
-      <br />
-      Or use arrows on the header to just switch topics.
+      There is a large grid of topics and tabs. Swipe left/right to switch tabs, or use arrows in the header to switch
+      topics while keeping tab constant.
     </div>
   );
 };
