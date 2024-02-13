c8e52cdcd195398a01edeae9df731dcba6cb7c01
Sitaram Iyer <sitaram@gmail.com>
2024-02-13 13:07:42 -0800

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,18 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* Google tag (gtag.js) - Google Analytics */}
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TX2GLY9R4W" />
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-TX2GLY9R4W`} />
       <Script
+        id="google-analytics-script" // Add an `id` attribute here
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
-            gtag('config', 'G-TX2GLY9R4W');
+            gtag('config', 'G-TX2GLY9R4W', {
+              page_path: window.location.pathname,
+            });
           `,
         }}
       />
