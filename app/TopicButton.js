d2d7071a7896476cee8192534091f85e5061edc2
Sitaram Iyer <sitaram@gmail.com>
2024-03-02 17:41:58 -0800

    misc

--- app/TopicButton.js
+++ app/TopicButton.js
@@ -23,24 +23,13 @@ const TopicButton = ({ handleTopicsDrawerToggle }) => {
             bottom: '30px',
             height: '70px',
             width: '70px',
+            lineHeight: '1rem',
             fontSize: '1rem',
             textTransform: 'none',
             boxShadow: `4px 4px 8px ${hexToRgba(theme.palette.primary.main, 0.7)}`,
-            '&:hover': {
-              backgroundColor: theme.palette.primary.dark + '!important', // Darker shade on hover
-              boxShadow: `0px 5px 15px ${hexToRgba(theme.palette.primary.main, 0.7)}`, // More pronounced shadow on hover
-            },
-            '&:active': {
-              backgroundColor: theme.palette.primary.light + '!important', // Lighter shade when button is clicked (active)
-              boxShadow: `0px 2px 5px ${hexToRgba(theme.palette.primary.main, 0.5)}`, // Less pronounced shadow when button is clicked
-            },
-            '&:focus': {
-              outline: `2px solid ${theme.palette.secondary.main}`, // Outline color when button is focused
-              outlineOffset: '2px',
-            }
           }}
         >
-          Topics
+          AI Topics
         </Fab>
       )}
     </>
