f090fe1c3046b4d9496bac9e535db7073a557ed8
Sitaram Iyer <sitaram@gmail.com>
2024-03-04 12:37:19 -0800

    yt

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,4 @@
 export const APPNAME = 'AI Trends';
 export const ALLTOPICS = 'All AI Topics';
 export const ALLTOPICS_TITLE = 'Artificial Intelligence';
-export const ALLTOPICS_YT_QUERY = 'Artificial Intelligence Trends';
+export const ALLTOPICS_YT_QUERY = 'Latest Artificial Intelligence Trends';
