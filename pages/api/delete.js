dda5274e093ae5b023e515f2b326320282752559
Sitaram Iyer <sitaram@gmail.com>
2024-03-05 15:45:15 -0800

    delete

---
+++ pages/api/delete.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import Redis from 'ioredis';
+import { prompts, shouldWebSearch, webSearchPrompt } from '../../server/prompts';
+
+dotenv.config(); // Load environment variables from .env file
+
+const redis = new Redis(process.env.REDIS_URL); // Redis connection URL
+
+export default async (req, res) => {
+  try {
+    const { topic, tab } = req.query;
+    if (!topic) return res.status(400).json({ error: 'Topic is required' });
+    if (!tab) return res.status(400).json({ error: 'Tab is required' });
+
+    const prompt = prompts[tab].replace('${topic}', topic) + (shouldWebSearch[tab] ? webSearchPrompt : '');
+    console.log('DELETE:', prompt);
+
+    const cacheKey = `prompt:${prompt}`;
+    await redis.del(cacheKey);
+
+    return res.status(200).json({ message: 'Deleted from Redis: ' + cacheKey });
+  } catch (error) {
+    console.error('Error deleting key:', error);
+    res.status(500).json({ error: 'An error occurred', details: error.message });
+  }
+};
