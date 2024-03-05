e12b1ebd9b387897abf4dd37157117bcd3cbc95f
Sitaram Iyer <sitaram@gmail.com>
2024-03-05 10:14:17 -0800

    sitemap

--- mksitemap.sh
+++ mksitemap.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# usage: ./mksitemap.sh > public/sitemap.xml; git commit; git push
 
 date=$(date +%Y-%m-%d)
 
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+echo "  <url>"
+echo "    <loc>http://aitrends.live/</loc>" | sed 's/\&/\&amp;/g'
+echo "    <lastmod>$date</lastmod>"
+echo "  </url>"
 
 awk -F"'" '/^        /{print $2}' app/topics.js | urlencode |
 while read -r topic; do
