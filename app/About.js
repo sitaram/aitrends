199e4746c1945421868143e42c8fc10b5fdfcdb2
Sitaram Iyer <sitaram@gmail.com>
2024-03-29 22:12:28 -0700

    about

--- app/About.js
+++ app/About.js
@@ -7,6 +7,13 @@ const About = ({ showAbout, setShowAbout }) => {
 
   const handleClose = () => setShowAbout(false);
 
+  const handleLinkedInPost = () => {
+    window.open(
+      'https://www.linkedin.com/posts/activity-7142703288986050561-oRre?utm_source=share&utm_medium=member_desktop',
+      '_blank'
+    );
+  };
+
   return (
     <Modal
       open={showAbout}
@@ -30,35 +37,49 @@ const About = ({ showAbout, setShowAbout }) => {
         }}
       >
         <Typography id="about-aitrends-title" variant="h5" component="h2" gutterBottom>
-          AITrends.live: Your AI Exploration Hub
+          AITrends.live: Your AI Hub
         </Typography>
+        <Divider sx={{ mb: 2 }} />
         <Typography id="about-aitrends-description" variant="body1" sx={{ mb: 2 }}>
-          Keeping pace with AI can be daunting. AITrends.live was created to simplify this challenge, offering a
-          comprehensive view across 200 AI topics and 25 facets each. From foundational concepts to future predictions,
-          technical deep-dives to innovative applications, ethical debates to the funding scene, AI Trends maps out the
-          entire AI landscape into a matrix of ideas.
+          I find it hard to keep up with AI. So I created AITrends, offering a comprehensive view across 200 AI topics
+          and 25 aspects of each topic. From foundational concepts to future predictions, technical deep-dives to
+          innovative applications, ethical debates to recently funding startups, AI Trends maps out the entire AI
+          landscape into a matrix of 5,000 ideas.
         </Typography>
         <Divider sx={{ mb: 2 }} />
         <Typography variant="body2" sx={{ mb: 2 }}>
-          This is a co-creation by Sitaram Iyer (sitaram at gmail) and ChatGPT. Through 2,500 chat messages in 3 weeks
-          spanning ideas, product, design, code, and data, we meticulously curated a resource that informs and hopefully
-          inspires. Whether you&apos;re exploring innovative use cases in your field, navigating career paths, or
-          looking for upskilling resources, AITrends.live is your go-to.
+          This is a co-creation by Sitaram Iyer (sitaram at gmail) and ChatGPT. Through 3,000 chat messages in 3 weeks,
+          spanning ideas, product, design, code, and data, even the domain name and color scheme, the two of us curated
+          a resource that informs and hopefully inspires. Whether you&apos;re exploring innovative use cases in your
+          field, researching career paths, or looking for upskilling resources, AITrends could be your starting point.
         </Typography>
         <Divider sx={{ mb: 2 }} />
         <Typography variant="body2" component="div" sx={{ mb: 2 }}>
-          Built for both enthusiasts and professionals, AITrends.live is a testament to how AI can transform our
-          understanding and engagement with technology for the common good. Dive into the AI revolution with us, your
-          journey into AI starts here.
+          Under the hood, this uses the OpenAI API with meta-prompting (ChatGPT generated tabs and prompts), recursive
+          summarization (overview tab is a summary of the other tabs), Bing API to freshen up the News, Events, and
+          Research tabs, YouTube API for a splash of fun, and is written in Next.js and hosted on Vercel.
+        </Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="body2" component="div" sx={{ mb: 2 }}>
+          Hope you enjoy learning AI (using AI)!
         </Typography>
         <Box sx={{ textAlign: 'right' }}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleLinkedInPost}
+            style={{ backgroundColor: theme.palette.primary.main }}
+          >
+            Read more
+          </Button>
+          &emsp;
           <Button
             variant="contained"
             color="primary"
             onClick={handleClose}
             style={{ backgroundColor: theme.palette.primary.main }}
           >
-            Got it
+            OK
           </Button>
         </Box>
       </Box>
